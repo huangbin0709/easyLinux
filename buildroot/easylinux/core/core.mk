07deed5cfd09edf17a666eea4add9070e3abaad3
huangbin0709 <523182570@qq.com>
2016-09-30 23:18:25 -0700

    optmize the mk

--- buildroot/easylinux/core/core.mk
+++ buildroot/easylinux/core/core.mk
@@ -11,7 +11,13 @@ CORE_INSTALL_TARGET = YES
 #CORE_CONF_OPTS +=
 #CORE_DEPENDENCIES +=
 
+ifeq ($(BR2_EASYLINUX_DEMO),y)
+	CORE_INC += -DINC_APP_DEMO
+	CORE_DEPENDENCIES += demo
+	CORE_APP_LIBS += libdemo.a
+endif
+
 CORE_CFLAGS += $(BR2_EASYLINUX_CFLAGS)
 #CORE_CFLAGS += 
-CORE_CONF_OPTS += -DCMAKE_C_FLAGS="$(CORE_CFLAGS)"
+CORE_CONF_OPTS += -DCMAKE_C_FLAGS="$(CORE_CFLAGS) $(CORE_INC) -Wall -Werror -fno-builtin -pthread -lm -lrt"
 $(eval $(cmake-package))
